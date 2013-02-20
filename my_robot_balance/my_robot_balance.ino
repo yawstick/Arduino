e29d3df0c4ab68230cbd3a7abfd3c9c2aaa0027e
yawstick <yawstick@gmail.com>
2013-02-19 21:02:58 -0500

    one more time

--- my_robot_balance/my_robot_balance.ino
+++ my_robot_balance/my_robot_balance.ino
@@ -1,6 +1,7 @@
 // Self balancing Mini robot with 4 motors.//
 //    Created by Arkadi Rafalovich    ///////
 //      Last Update 16.01.2013//////////////
+/// handyman version added servo on pin 9  ///
 //////////////////////////////////////
 // libraries in use:
 // for running MPU 6050 6/21/2012 by Jeff Rowberg
